1f68754e39316189cad3d13ce6a0f4efb149c21b
师飞 <sferic@163.com>
2019-03-19 22:49:41 +0800

    State ViewController

--- UnidirectionDataFlow/UnidirectionDataFlow/TableViewController.swift
+++ UnidirectionDataFlow/UnidirectionDataFlow/TableViewController.swift
@@ -18,10 +18,36 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    // 内嵌枚举
+    /// 内嵌枚举
     enum Section: Int {
         case input = 0, todos, max
     }
+    /// 内嵌结构体（将两个变量进行简单地封装）
+    struct State {
+        let todos: [String]
+        let text:String
+    }
+    // 这样就有一个统一按照状态更新UI的地方了
+    var state = State(todos: [], text: "") {
+        didSet {
+            if oldValue.todos != state.todos {
+                tableView.reloadData()
+                title = "TODO - (\(state.todos.count))"
+            }
+            
+            if oldValue.text != state.text {
+                let isItemLengthEnough = state.text.count >= 3
+                navigationItem.rightBarButtonItem?.isEnabled = isItemLengthEnough
+                
+                let inputIndexPath = IndexPath(item: 0, section: Section.input.rawValue)
+                let inputCell = tableView.cellForRow(at: inputIndexPath) as? TableViewInputCell
+                inputCell?.textFiled.text = state.text
+            }
+            // 这里我们将新值和旧值进行了一些比较，以避免不必要的UI更新，就可以将原来的UI操作换成对state的操作了
+        }
+        
+    }
+    
     var todos = [String]()
     
     override func viewDidLoad() {
@@ -31,24 +57,33 @@ class TableViewController: UITableViewController {
         pressedBtn.isEnabled = false
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: pressedBtn)
         pressedBtn.addTarget(self, action: #selector(addButtonPressed(_:)), for: .touchUpInside)
-        ToDoStore.shared.getToDoItems { (data) in
+        
+        // 变更前
+       /* ToDoStore.shared.getToDoItems { (data) in
             self.todos += data
             self.title = "TODO - (\(self.todos.count))"
             self.tableView.reloadData()
+        }*/
+        // 变更后
+        ToDoStore.shared.getToDoItems { (data) in
+            self.state = State(todos: self.state.todos + data, text: self.state.text)
         }
         tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "reuseIdentifier")
     }
     
     // 添加代办
     @objc func addButtonPressed(_ btn: UIButton) {
-        let inputIndexPath = IndexPath(row: 0, section: Section.input.rawValue)
+        // 变更前
+        /*let inputIndexPath = IndexPath(row: 0, section: Section.input.rawValue)
         guard let inputcell = tableView.cellForRow(at: inputIndexPath) as? TableViewInputCell, let text = inputcell.textFiled.text else {
             return
         }
         todos.insert(text, at: 0)
         inputcell.textFiled.text = ""
         title = "TODO - (\(todos.count))"
-        tableView.reloadData()
+        tableView.reloadData()*/
+        // 变更后
+        state = State(todos: [state.text] + state.todos, text: "")
     }
     
     
@@ -94,25 +129,29 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 移除代办
-        guard indexPath.section == Section.todos.rawValue else {
+        // 变更前
+        /*guard indexPath.section == Section.todos.rawValue else {
             return
         }
         todos.remove(at: indexPath.row)
         title = "TODO - (\(todos.count))"
-        tableView.reloadData()
+        tableView.reloadData()*/
+        // 变更后
+        guard indexPath.section == Section.todos.rawValue else {
+            return
+        }
+        let newTodos = Array(state.todos[..<indexPath.row] + state.todos[(indexPath.row + 1)...])
+        state = State(todos: newTodos, text: state.text)
     }
 }
 
 extension TableViewController: TableViewInputCellDelegate {
     func inputChanged(cell: TableViewInputCell, text: String) {
-        let isItemLengthEnough = text.count >= 3
-        navigationItem.rightBarButtonItem?.isEnabled = isItemLengthEnough
+        // 变更前
+        /*let isItemLengthEnough = text.count >= 3
+        navigationItem.rightBarButtonItem?.isEnabled = isItemLengthEnough*/
+        // 变更后
+        state = State(todos: state.todos, text: text)
     }
 }
 
-/*
- 1.UI 相关的代码散落各处 - 重载 tableView 和设置 title 的代码出现了三次，设置右上 button 的 isEnabled 的代码存在于 extension 中，添加新项目时我们先获取了输入的 cell，然后再读取 cell 中的文本。这些散落在各处的 UI 操作将会成为隐患，因为你可能在代码的任意部分操作这些 UI，而它们的状态将随着代码的复杂变得“飘忽不定”。
- 2.因为 1 的状态复杂，使得 View Controller 难以测试 - 举个例子，如果你想测试 title 的文字正确，你可能需要手动向列表里添加一个待办事项，这涉及到调用 addButtonPressed，而这个方法需要读取 inputCell 的文本，那么你可能还需要先去设置这个 cell 中 UITextField 的 text 值。当然你也可以用依赖注入的方式给 add 方法一个文本参数，或者将 todos.insert 和之后的内容提取成一个新的方法，但是无论怎么样，对于 model 的操作和对于 UI 的更新都没有分离 (因为毕竟我们写的就是“胶水代码”)。这正是你觉得 View Controller 难以测试的最主要原因。
- 3.因为 2 的难以测试，最后让 View Controller 难以重构 - 状态和 UI 复杂度的增加往往会导致多个 UI 操作维护着同一个变量，或者多个状态变量去更新同一个 UI 元素。不论是哪种情况，都让测试变得几乎不可能，也会让后续的开发人员 (其实往往就是你自己！) 在面对复杂情况下难以正确地继续开发。Massive View Controller 最终的结果常常是牵一发而动全身，一个微小的改动可能都需要花费大量的时间进行验证，而且还没有人敢拍胸脯保证正确性。这会让项目逐渐陷入泥潭。
-
- */
--- UnidirectionDataFlow/UnidirectionDataFlow/ViewController.swift
+++ UnidirectionDataFlow/UnidirectionDataFlow/ViewController.swift
@@ -4,11 +4,13 @@
 //
 //  Created by 师飞 on 2019/3/19.
 //  Copyright © 2019年 师飞. All rights reserved.
-//  单向数据流动的函数式View Controller
-/* 这个项目可以从网络加载待办事项，我们通过输入文本进行添加，或者点击对应条目进行删除
-打开应用后加载已有待办列表时花费了一些时间，一般来说，我们会从网络请求进行加载，这应该是一个异步操作。在示例项目里，我们不会真的去进行网络请求，而是使用一个本地存储来模拟这个过程。
-标题栏的数字表示当前已有的待办项目，随着待办的增减，这个数字会相应变化。
-可以使用第一个 cell 输入，并用右上角的加号添加一个待办。我们希望待办事项的标题长度至少为三个字符，在不满足长度的时候，添加按钮不可用。*/
+/*基于 State 的 View Controller
+
+通过提取 State 统合 UI 操作
+上面的三个问题其实环环相扣，如果我们能将 UI 相关代码集中起来，并用单一的状态去管理它，就可以让 View Controller 的复杂度降低很多。我们尝试看看！
+
+在这个简单的界面中，和 UI 相关的 model 包括待办条目 todos (用来组织 table view 和更新标题栏) 以及输入的 text (用来决定添加按钮的 enable 和添加 todo 时的内容)。我们将这两个变量进行简单的封装，在 TableViewController 里添加一个内嵌的 State 结构体：*/
+
 import UIKit
 
 class ViewController: UIViewController {
